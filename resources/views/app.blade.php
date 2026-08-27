ff388990d321de48beb7cc64fa6ea9d9a735d987
DarkCodereaper <144976844+Codereaper101526@users.noreply.github.com>
2026-08-27 14:04:42 +0800

    Use Bunny CDN fonts & fix deploy message
    Switch font loading from the laravel-vite-plugin to direct Bunny CDN links in resources/views/app.blade.php (adds preconnect and CSS href, removes @fonts and apple-touch-icon). Remove the fonts plugin import and configuration from vite.config.ts. Also fix a stray character in the deployment echo message in .github/workflows/deploy.yml.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,9 +33,8 @@
 
         <link rel="icon" href="/logo.webp" sizes="any">
         <link rel="icon" href="/logo.webp" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
-        @fonts
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
